bc5ecf1c0fece5f604e6159020e10bf3723f1c4e
nngtkhngoc <khanhngoc230705@gmail.com>
2025-03-15 16:10:21 +0700

    scroll to top

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,28 @@
+import { ChevronsUp } from "lucide-react";
+import { useState, useEffect } from "react";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisble] = useState(false);
+
+  const handleVisibleButton = () => {
+    setIsVisble(window.pageYOffset > 50);
+  };
+
+  const handleScrollUp = () => {
+    window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleVisibleButton);
+  }, []);
+  return (
+    <button
+      className={`bg-black p-2 rounded-full fixed bottom-10 right-10 cursor-pointer animate-jump ${
+        isVisible ? "block" : "hidden"
+      }`}
+      onClick={handleScrollUp}
+    >
+      <ChevronsUp className="text-white w-6 h-6" />
+    </button>
+  );
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
 import FooterBottom from "./FooterBootom.tsx";
+import ScrollToTop from "./ScrollToTop.tsx";
 
 export default function Layout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <ScrollToTop />
       <Footer />
       <FooterBottom />
     </div>
