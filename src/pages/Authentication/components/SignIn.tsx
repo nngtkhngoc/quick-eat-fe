f094990a0ba46ad7189f2570effc2bb7e4ae0211
nngtkhngoc <khanhngoc230705@gmail.com>
2025-04-16 21:50:35 +0700

    fix form validation

--- src/pages/Authentication/components/SignIn.tsx
+++ src/pages/Authentication/components/SignIn.tsx
@@ -115,15 +115,14 @@ export default function SignIn() {
         >
           Forget Password?
         </Link>
+        <button
+          type="submit"
+          className=" w-full relative bg-red-600 py-3 px-9 font-poppins border-transparent text-white text-semibold text-[14px] cursor-pointer before:absolute before:w-1 before:bg-black before:h-1 before:top-0 before:left-0 before:-z-5 hover:z-10 hover:before:w-full hover:before:h-full before:transition-all before:duration-500"
+          onClick={handleSignIn}
+        >
+          {loading ? "Loading..." : "SIGN IN"}
+        </button>
       </form>
-
-      <button
-        type="submit"
-        className=" w-full relative bg-red-600 py-3 px-9 font-poppins border-transparent text-white text-semibold text-[14px] cursor-pointer before:absolute before:w-1 before:bg-black before:h-1 before:top-0 before:left-0 before:-z-5 hover:z-10 hover:before:w-full hover:before:h-full before:transition-all before:duration-500"
-        onClick={handleSignIn}
-      >
-        {loading ? "Loading..." : "SIGN IN"}
-      </button>
     </div>
   );
 }
--- src/pages/Authentication/components/SignUp.tsx
+++ src/pages/Authentication/components/SignUp.tsx
@@ -191,13 +191,13 @@ export default function SignUp() {
         >
           Forget Password?
         </Link>
+        <button
+          className=" w-full relative bg-red-600 py-3 px-9 font-poppins border-transparent text-white text-semibold text-[14px] cursor-pointer before:absolute before:w-1 before:bg-black before:h-1 before:top-0 before:left-0 before:-z-5 hover:z-10 hover:before:w-full hover:before:h-full before:transition-all before:duration-500"
+          onClick={handleSignUp}
+        >
+          {loading ? "Loading..." : "SIGN UP"}
+        </button>
       </form>
-      <button
-        className=" w-full relative bg-red-600 py-3 px-9 font-poppins border-transparent text-white text-semibold text-[14px] cursor-pointer before:absolute before:w-1 before:bg-black before:h-1 before:top-0 before:left-0 before:-z-5 hover:z-10 hover:before:w-full hover:before:h-full before:transition-all before:duration-500"
-        onClick={handleSignUp}
-      >
-        {loading ? "Loading..." : "SIGN UP"}
-      </button>
     </div>
   );
 }
